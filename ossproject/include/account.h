99636f43bfe83e024a315b1e8dc706deaa37ced1
immanuelk1m <98140860+immanuelk1m@users.noreply.github.com>
2023-05-10 01:18:50 +0900

    update account.h

--- ossproject/include/account.h
+++ ossproject/include/account.h
@@ -21,7 +21,7 @@ struct Account {
 	map<string, Stock> stockOwned; // 보유 주식 데이터
 
 	int purchaseAmount = 0; // 매입 금액
-	int totalStockValue = 0; //
+	int totalStockValue = 0; // 평가 금
 	int asset = 0; // 총 자산
 	int cash = 0; // 예수금
 	int assetReturnRatio = 0; // 계좌 수익률
