21a90c5d18540c87d803566edb543719b34d3179
immanuelk1m <98140860+immanuelk1m@users.noreply.github.com>
2023-05-10 00:37:14 +0900

    main update

--- ossproject/main.cpp
+++ ossproject/main.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 int main(int argc, char **argv) 
 {
+  cin >> c ;
+  
+  // load 함수 구현
+  // 각 기능 단축키 설명
+
+  do{
+    switch (c) 
+    {
+      case 'C':
+        break;
+
+      case 'R':
+        break;
+
+      case 'U':
+        break;
+
+      case 'D':
+        break;
+    }
+  }while (c != 'q');
+
   return 0;
 }
