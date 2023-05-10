67ac55bbbbb88be16409e93a3ef0740a872009e6
immanuelk1m <kse0119@naver.com>
2023-05-10 12:07:34 +0900

    update main

---
+++ ossproject/include/data.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include "account.h"
+
+void loadUserStockData(account u);
+void loadUserData(vector<account> users);
+void loadUserStockData(vector<account> users);
+
--- ossproject/main.cpp
+++ ossproject/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "account.h"
+#include "data.h"
 
 using namespace std;
 
 int main(int argc, char **argv) 
 {
+  char c;
   cin >> c ;
-  
+  vector<account> users;
+  load(vector<account> users);
   // load 함수 구현
   // 각 기능 단축키 설명
 
