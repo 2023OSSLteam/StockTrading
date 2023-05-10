931a932833a5aad5c65d9fbaccfcf17fe2d379a0
immanuelk1m <kse0119@naver.com>
2023-05-10 18:26:17 +0900

    update data.cpp

--- ossproject/main.cpp
+++ ossproject/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 #include "account.h"
-#include "data.h"
+
 
 using namespace std;
 
@@ -10,8 +10,9 @@ int main(int argc, char **argv)
 {
   char c;
   cin >> c ;
+  map<vector> stocks;
   vector<account> users;
-  load(vector<account> users);
+  loadUserData(vector<account> users);
   // load 함수 구현
   // 각 기능 단축키 설명
 
