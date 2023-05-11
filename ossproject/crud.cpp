4246027e8db1ed0f47e210c2129e0c8c810e428c
knine123 <130415523+knine123@users.noreply.github.com>
2023-05-11 21:46:41 +0900

    Update crud.cpp

--- ossproject/crud.cpp
+++ ossproject/crud.cpp
@@ -1,1 +1,18 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include "data.h"
 
+
+void menu(){
+cout<<"      <메뉴>"      <<endl;
+cout<<"C: 회원가입"<<endl;
+cout<<"R: 나의 정보 보기"<<endl;
+cout<<"U: 나의 정보 수정"<<endl;
+cout<<"D: 탈퇴"<<endl;
+cout<<"q: 종료"<<endl;
+
+};
