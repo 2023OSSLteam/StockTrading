b0d50c28921ae2fcccaa6d7e726af93c9edad186
immanuelk1m <kse0119@naver.com>
2023-05-12 23:53:30 +0900

    update portfolio

--- ossproject/portfolio.cpp
+++ ossproject/portfolio.cpp
@@ -1,13 +1,21 @@
+#include <Windows.h>
+#include <stdio.h>
 #include <iostream>
 #include "account.h"
 #include "extrafunction.h"
 
+
 void viewPortfolio(account u)
-{
-    
+{   
     for (const auto& [symbol, stk] : u->stockOwned) 
     {
-        cout << symbol << " " << stk->purchase_price / u->purchaseAmount << endl;
+        cout << symbol << " " << (stk->purchase_amount / u->assetAmount)*100 << endl;
     }
-    cout << u->cash << endl;
+    //cout << u->cash << endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+    for(int i=0;i<100;i++)
+        cout << "/■" ;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    //cout << u->assetAmount << endl;
+    cout << "cash " << (float(u->cash)/float(u->assetAmount))*100.0 << endl;
 }
