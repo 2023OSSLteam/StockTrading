bf9af6f92fc1d47484260f78e803db4551792478
knine123 <130415523+knine123@users.noreply.github.com>
2023-05-13 23:39:23 +0900

    Update crud.cpp
    CRU 기능을 구현하였습니다.
    C: 회원가입
    R: 자신의 정보 확인
    U: 계좌 변경, 예수금 이동

--- ossproject/crud.cpp
+++ ossproject/crud.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <cstring>
 #include "data.h"
 
 
@@ -16,3 +17,290 @@ cout<<"D: 탈퇴"<<endl;
 cout<<"q: 종료"<<endl;
 
 };
+
+void create(vector<account> *users, map<string, vector<int>> stockdata){//회원가입
+
+account newN=new Account;
+
+//기본
+cout<<"이름을 작성하시오.: ";
+cin>>newN->owner_name;
+
+cout<<"계좌번호를 작성하시오.: ";
+cin>>newN->accountID;
+
+cout<<"계좌 비밀번호를 작성하시오.: ";
+cin>>newN->password;
+
+cout<<"예수금은 얼마입니까?";
+cin>>newN->cash;
+
+
+//stock
+char y_n;
+cout<<"가지고 있는 주식은 있습니까?(예: y 아니요: n)";
+cin>>y_n;
+
+
+
+    while(y_n=='Y' || y_n=='y'){
+        stock newstock= new Stock;
+        string a;//회사 이름
+
+        cout<<"가지고 있는 주식의 기업은?: ";
+        cin>>newstock->name;
+
+
+        cout<<"그 주식의 매입 가격은?: ";
+        cin>>newstock->purchase_price;
+
+
+        cout<<"그 주식의 보유수량은?: ";
+        cin>>newstock->holding_amount;
+
+
+        newstock->purchase_amount=newstock->purchase_price*newstock->holding_amount;
+
+
+        newstock->current_price = stockdata[newstock->name].back();
+
+
+        newstock->evaluation_amount = newstock->current_price*newstock->holding_amount;
+
+
+
+        newN->stockOwned[a]=newstock;
+
+        while(1){
+            cout<<"그 외의 주식은 더 있습니까?(예:y 아니오:n)";
+            cin>>y_n;
+
+            if(y_n=='N' || y_n=='n'){
+                break;
+            }
+            else if(y_n=='y' || y_n=='Y'){
+                break;
+            }
+            else if(cin.fail()){
+                cout<<"잘못입력하였습니다."<<endl;
+                cin.clear();
+                cin.ignore(256,'\n');
+            }
+            else{
+                cout<<"잘못입력하였습니다."<<endl;
+                cin.clear();
+                cin.ignore(256,'\n');
+            }
+        }
+        if(y_n=='N' || y_n=='n'){
+            break;
+        }
+
+}
+//stock를 이용하여 나머지 account채우기
+
+for(auto const& a : newN->stockOwned){
+    auto const& key=a.first;
+    auto const& val=a.second;
+    newN->purchaseAmount += val->purchase_amount ;
+    newN->totalStockValue += val->evaluation_amount;
+    }
+newN->assetAmount = newN->purchaseAmount + newN->cash;
+newN->assetValue = newN->totalStockValue + newN->cash;
+            
+newN->assetReturnRatio = newN->assetValue/newN->assetAmount;
+newN->assetReturnValue = newN->assetValue - newN->assetAmount;
+
+users->push_back(newN);//users에 새로 만든 회원 넣기
+
+}
+
+
+
+void read(vector<account> users){//자기 정보 읽기
+    string name;
+    string number;//계좌번호
+    string pass;//패스워드
+    int y_n=0;
+
+    cout<<"서명, 계좌번호, 계좌비밀번호를 작성하시오."<<endl;
+    cout<<"서명: ";
+    cin>>name;
+    cout<<"계좌번호: ";
+    cin>>number;
+    cout<<"계좌비밀번호: ";  
+    cin>>pass;
+
+
+    for(int i=0; i<users.size(); i++){
+
+        if(users[i]->owner_name.compare(name)==0 && users[i]->accountID.compare(number)==0 && users[i]->password.compare(pass)==0){
+
+            cout<<"user: "+users[i]->owner_name<<endl;
+            cout<<"계좌번호: "+users[i]->accountID<<endl;
+            for(auto const& a : users[i]->stockOwned){
+                
+                auto const& val=a.second;
+                cout<<"기업: ";
+                cout<<val->name<<endl;
+                cout<<"현재 금액: ";
+                cout<<val->current_price<<endl;
+                cout<<"보유 수량: ";
+                cout<<val->holding_amount<<endl;
+                cout<<"평가금액: ";
+                cout<<val->evaluation_amount<<endl;
+            }
+            y_n++;
+            break;
+        }
+    }
+
+    if(y_n == 0){
+        cout<<"정보를 잘못 입력하였거나 없는 정보입니다. 다시 시도해주세요."<<endl;
+    }
+}
+
+
+void update(vector<account> users){//예수금(빼거나 더하는 형식으로), 계좌 비밀번호, 계좌 번호(다른 계좌로 바꾸는 형식), 
+    string name, num, password, newnum, newpassword;
+    int check;//예수금 더할지 말지
+    int number;
+    int move;
+    int out=0;
+    while(1){
+        cout<<"0. 나가기"<<endl<<"1.예수금 이동"<<endl<<"2.계좌변경"<<endl<<"무엇을 하시겠습니까?: ";
+        cin>>number;
+        if(number==1){
+            cout<<"이름을 입력하시오"<<endl;
+            cin>>name;
+            cout<<"계좌번호를 입력하십시오: "<<endl;
+            cin>>num;
+            cout<<"계좌비밀번호를 입력하시오: "<<endl;
+            cin>>password;
+
+            for(int i=0; i<users.size();i++){
+                if(users[i]->owner_name.compare(name)==0 && users[i]->accountID.compare(num)==0 && users[i]->password.compare(password)==0){
+                    cout<<"금액을 추가할 것입니까? 뺄 것입니까?(추가:1, 제거:0)";
+                    cin>>check;
+                    if(check==1){
+                        cout<<"얼마를 넣을 것입니까?";
+                        cin>>move;
+                        if(move<0){
+                            cout<<"음수를 넣을 수 없습니다. 양수를 입력하세요."<<endl;
+                            break;
+                        }
+                        else if(move==0){
+                            cout<<"0원을 넣을 수 없습니다. 양수를 입력하세요."<<endl;
+                            break;
+                        }
+                        else if(move>0){
+                            users[i]->cash+=move;
+                            cout<<"충전되었습니다."<<endl;
+                            break;
+                        }
+                        else if(cin.fail()){
+                            cout<<"숫자를 입력하시오."<<endl;
+                            break;
+                        }
+                        else{
+                            cout<<"알 수 없는 오류"<<endl;
+                            break;
+                        }
+                    }
+                    else if(check==0){
+                        cout<<"얼마를 이동하시겠습니까?(예수금:"+users[i]->cash<<"): ";
+                        cin>>move;
+                        if(move>users[i]->cash){
+                            cout<<"금액을 초과하였습니다."<<endl;
+                            break;
+                        }
+                        else if(move<0){
+                            cout<<"양수를 넣어야 합니다."<<endl;
+                            break;
+                        }
+                        else if(cin.fail()){
+                            cout<<"정수가 아닌 수, 혹은 문자는 입력받을 수 없습니다."<<endl;
+                            break;
+                        }
+                        else if(move>=0 && move<=users[i]->cash){
+                            users[i]->cash=users[i]->cash-move;
+                            break;
+                        }
+                        else{
+                            cout<<"알 수 없는 오류"<<endl;
+                            break;
+                        }
+                    }
+                    else if(cin.fail()){
+                        cout<<"숫자를 입력하시오."<<endl;
+                    }
+                    else{
+                        cout<<"알 수 없는 오류"<<endl;
+                    }
+
+
+                    
+                }
+
+            }
+        }
+        else if(number==2){
+            cout<<"이름을 입력하시오"<<endl;
+            cin>>name;
+            cout<<"계좌번호를 입력하십시오: "<<endl;
+            cin>>num;
+            cout<<"계좌비밀번호를 입력하시오: "<<endl;
+            cin>>password;
+
+            for(int i=0; i<users.size(); i++){
+                if(users[i]->owner_name.compare(name)==0 && users[i]->accountID.compare(num)==0 && users[i]->password.compare(password)==0){
+                    cout<<"변경할 계좌번호를 입력하십시오: "<<endl;
+                    cin>>newnum;
+                    cout<<"변경할 계좌비밀번호를 입력하시오: "<<endl;
+                    cin>>newpassword;
+                    
+                    string a= "./data/userdata/"+users[i]->accountID;
+                    a+=".txt";
+                    cout<<a<<endl;
+                    const char *b= a.c_str();
+                    cout<<remove(b);
+
+                    users[i]->accountID=newnum;
+                    users[i]->password=newpassword;
+                    break;
+                }
+                else{
+                    cout<<"정보가 없습니다."<<endl;
+                }
+            }
+        }
+        else if(cin.fail()){
+            cout<<"잘못입력하였습니다."<<endl;
+            cin.clear();
+            cin.ignore(256,'\n');
+
+        }
+        else if(number==0){
+            break;
+        }
+        else{
+            cout<<"잘못입력하였습니다."<<endl;
+            cin.clear();
+            cin.ignore(256,'\n');
+        }
+    cout<<"끝내시겠습니까?(예:1 아니오:0): ";
+    cin>>out;
+    if(out==1){
+        break;
+    }
+    }
+
+}
+
+
+
+void delte(){//탈퇴
+
+
+
+}
