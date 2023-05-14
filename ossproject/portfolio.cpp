fd227c4f149e7b5c98e882b4c76096105b9f8d2f
immanuelk1m <kse0119@naver.com>
2023-05-15 02:32:12 +0900

    portfolio.cpp update

--- ossproject/portfolio.cpp
+++ ossproject/portfolio.cpp
@@ -1,23 +1,52 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <time.h>
 #include "account.h"
 #include "extrafunction.h"
 
+void setColor(unsigned short text, unsigned short back)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), text | (back << 4));
+}
 
 void viewPortfolio(account u)
 {   
+    srand(time(0));
+
     for (const auto& [symbol, stk] : u->stockOwned) 
     {
-        cout << symbol << " " << (stk->purchase_amount / u->assetAmount)*100 << endl;
+        int rate = (stk->purchase_amount / u->assetAmount)*100;
+        cout << symbol << " " << rate <<"%" << endl;
+
+        unsigned short text = rand() % 16;
+        unsigned short back = (rand() % 15) + 1;
+        setColor(text,back);
+        for(int i=0;i<rate;i++)
+        {
+            cout << " " ;
+        }
+        
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        cout << endl;
     }
-    //cout << u->cash << endl;
-    /*
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
-    for(int i=0;i<100;i++)
-        cout << "/■" ;
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-    */
+    int cashrate = (float(u->cash)/float(u->assetAmount))*100.0;
+    
+    cout << "Cash " << cashrate << "%" << endl;
+
+    unsigned short text = rand() % 16;
+    unsigned short back = (rand() % 15) + 1;
+
+    setColor(text,back);
+    for(int i=0;i<cashrate;i++)
+    {
+        cout << " " ;
+    }
+    
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    cout << endl;
+         
     //cout << u->assetAmount << endl;
-    cout << "cash " << (float(u->cash)/float(u->assetAmount))*100.0 << endl;
+    
 }
