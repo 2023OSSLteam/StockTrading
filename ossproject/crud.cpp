6ac1a4437f6503468d77dd0007a31a5e0e2f8bdb
knine123 <130415523+knine123@users.noreply.github.com>
2023-05-17 01:56:51 +0900

    Update crud.cpp

--- ossproject/crud.cpp
+++ ossproject/crud.cpp
@@ -13,6 +13,7 @@ cout<<"      <메뉴>      "<<endl;
 cout<<"C: 회원가입"<<endl;
 cout<<"R: 나의 정보 보기"<<endl;
 cout<<"U: 나의 정보 수정"<<endl;
+cout<<"B: 주식 구매"<<endl;
 cout<<"S: 주식 판매"<<endl;
 cout<<"D: 탈퇴"<<endl;
 cout<<"q: 종료"<<endl;
