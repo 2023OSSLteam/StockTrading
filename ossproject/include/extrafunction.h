43e514233137164e4225a9c903a734b87d53a481
knine123 <130415523+knine123@users.noreply.github.com>
2023-05-20 01:02:42 +0900

    Update extrafunction.h

--- ossproject/include/extrafunction.h
+++ ossproject/include/extrafunction.h
@@ -4,3 +4,4 @@ void calculateRSI(map<string, vector<int>>& stocks, string stockName);
 void printNowStock(map<string, vector<int>>& stocks, int rank);
 void fallSearch(map<string, vector<int>>& stocks, int fallRate);
 void sellALL(vector<account>& users);
+void graph(map<string, vector<int>> stockdata);
